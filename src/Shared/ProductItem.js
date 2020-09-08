981513332e9faa384394f94db7228a3236d2f997
mjy18 <1110ymj@sookmyung.ac.kr>
2020-09-08 15:43:36 +0900

    productlist

--- src/Shared/ProductItem.js
+++ src/Shared/ProductItem.js
@@ -1,25 +1,41 @@
 import React from "react";
 import { IconContext } from "react-icons";
-
 import { FaHeart } from "react-icons/fa";
-
 import background from "../assets/background.png";
 
 const ProductItem = () => {
   return (
-    <div>
-      item
-      <img src={background} />
-      <span className="location">거래위치</span>
-      <span className="upload">9시간전</span>
-      <span className="like">50</span>
-      <IconContext.Provider value={{ className: "heart" }}>
-        <div>
-          <FaHeart />
-        </div>
-      </IconContext.Provider>
-    </div>
-  );
+        <table className="Productitem">
+            <thead>
+            </thead>
+            <tbody>
+                <tr>
+                    <td rowspan="5"><img src={background} alt="background" className="backgroundimg"/></td>
+                </tr>
+                <tr>
+                    <td colspan="2" rowspan= "2"className="itemname">상품제목 글자는 최대 2줄</td>
+                </tr>
+                <tr></tr>
+                <tr>
+                    <td className="location">거래위치</td> <td className="upload">9시간 전</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="like">
+                        <IconContext.Provider value={{ className: "heart" }}>
+                            <div>
+                                <FaHeart />
+                                <span className="like"> 50</span>
+                            </div>
+                        </IconContext.Provider>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr><td colspan="3"></td></tr>
+            </tfoot>
+        </table>
+    );
 };
 
 export default ProductItem;
