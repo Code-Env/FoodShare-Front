31eeaf991a5c12c6356bea67f100a860a05d2f0a
mjy18 <1110ymj@sookmyung.ac.kr>
2020-09-08 17:44:49 +0900

    post

---
+++ src/pages/Post/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PostContainer from "../../containers/PostContainer";
+
+const Post = () => {
+  return(
+    <div>
+      <PostContainer/>
+    </div>
+  );
+};
+
+export default Post;
