ae73d9f98f2879a8aa7677d850caf1fd3ca0c0cb
kimtaegwan <ab154@naver.com>
2020-09-09 20:50:36 +0900

    Edit header

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,26 +1,23 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { IoMdHome } from "react-icons/io";
+
 const Footer = () => {
   return (
-    <div className="topnav">
-      <div id="myNav" class="overlay">
-        <a href="/" className="closebtn">
-          &times;
-        </a>
-        <div className="overlay-content">
-          <a href="#About">About</a>
-          <a href="#Services">Services</a>
-          <a href="#Clients">Clients</a>
-          <a href="#Contact">Contact</a>
-        </div>
+    <div className="bottomnav">
+      <div>menu</div>
+      <div>menu</div>
+      <div>
+        <Link to="/">
+          <IconContext.Provider value={{ className: "home_ico" }}>
+            <IoMdHome />
+          </IconContext.Provider>
+        </Link>
       </div>
-      <span style={{ marginLeft: "16px", fontSize: "30px", cursor: "pointer" }}>
-        &#9776;
-      </span>
-      <div className="topnav-right">
-        <a href="#search">Search</a>
-        <a href="#about">About</a>
-      </div>
+      <div>menu</div>
+      <div>menu</div>
     </div>
   );
 };
