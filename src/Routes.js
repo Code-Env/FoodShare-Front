92360dcdc6b12529502400fe7794d58f9e5931ce
kimtaegwan <ab154@naver.com>
2020-09-13 19:38:35 +0900

    Login Page add & edit

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import Main from "pages/Main";
 import ProductInfo from "pages/ProductInfo";
 import ChattingList from "pages/ChattingList";
+import Login from "pages/Login";
 
 const Routes = () => {
   return (
@@ -15,6 +16,7 @@ const Routes = () => {
         <Route exact path="/" component={Main} />
         <Route path="/product" component={ProductInfo} />
         <Route path="/chattinglist" component={ChattingList} />
+        <Route path="/login" component={Login} />
       </Switch>
       {/* </HashRouter> */}
     </>
---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginComponent from "Componets/Login";
+
+const LoginContainer = () => {
+  return <LoginComponent />;
+};
+
+export default LoginContainer;
