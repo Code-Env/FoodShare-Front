32e64a0ea13b1cb62a2881d742e6ac1b149d6345
kimtaegwan <ab154@naver.com>
2020-11-16 03:52:54 +0900

    Edit favourit button

--- src/containers/ProductInfoContainer.js
+++ src/containers/ProductInfoContainer.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductInfoComponent from "Componets/ProductInfoComponent";
 // import ProductInfoComponent from "Componets/ProductInfoComponent/asd";
 
 const ProductInfoContainer = () => {
-  return <ProductInfoComponent />;
+  const [favouriteCount, setFavouriteCount] = useState(100);
+  const [heart, setHeart] = useState(false);
+  const favourite = () => {
+    if (!heart) {
+      setFavouriteCount(favouriteCount + 1);
+    } else {
+      setFavouriteCount(favouriteCount - 1);
+    }
+    setHeart(!heart);
+  };
+  return (
+    <ProductInfoComponent
+      favouriteCount={favouriteCount}
+      heart={heart}
+      favourite={favourite}
+    />
+  );
 };
 
 export default ProductInfoContainer;
