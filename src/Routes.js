ac3514a39ea2215faabf396c67f241e14d1a903a
kimtaegwan <ab154@naver.com>
2021-01-29 02:15:31 +0900

    Set chat and Add redux, redux-logger

--- src/Routes.js
+++ src/Routes.js
@@ -7,8 +7,16 @@ import ProductInfo from "pages/ProductInfo";
 import ChattingList from "pages/ChattingList";
 import Login from "pages/Login";
 import Signup from "pages/Signup";
+import Chat from "Componets/ChatComponent";
 
-const Routes = () => {
+const Routes = ({
+  chatReducer,
+  mySocketId,
+  leaveChatroom,
+  enterChatroom,
+  sendChat,
+  clearChat,
+}) => {
   return (
     <>
       {/* <HashRouter basename="/"> */}
@@ -19,8 +27,25 @@ const Routes = () => {
         <Route path="/chattinglist" component={ChattingList} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route
+          path="/chat/:id"
+          render={props => (
+            <Chat
+              chatReducer={chatReducer}
+              mySocketId={mySocketId}
+              leaveChatroom={leaveChatroom}
+              enterChatroom={enterChatroom}
+              sendChat={sendChat}
+              clearChat={clearChat}
+            />
+          )}
+        />
       </Switch>
       {/* </HashRouter> */}
+      {/*
+        채팅 참고
+        https://gaemi606.tistory.com/56
+      */}
     </>
   );
 };
